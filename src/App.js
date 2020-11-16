b9ec1d4c24a353a5c0af9cd7902bcb964bb8cd95
Guido Westra <westra.guido@gmail.com>
2020-11-16 14:30:43 +0100

    1.1 updateH1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from "./logo.svg";
+import Title from "./components/title";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title />
+      </main>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>This moving thing above me is pretty cool :)</p>
